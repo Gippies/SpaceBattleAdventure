d4b341fb692cbcc836cf79d59ffa80cfd3cf77d5
Joshua Patterson <jleegippies@gmail.com>
2020-03-25 16:25:20 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    private static readonly float speed = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * speed;
+        transform.Translate(velocity * Time.deltaTime);
+    }
+}
