a80c186024c91a80d9418302b8fa631cd8dfa524
Joshua Patterson <jleegippies@gmail.com>
2020-03-25 16:39:41 -0500

    Set bounds for the area the player can move

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
-    private static readonly float speed = 10.0f;
+    private const float Speed = 10.0f;
     
     // Start is called before the first frame update
     void Start() {
@@ -13,7 +13,8 @@ public class PlayerBehavior : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * speed;
+        Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * Speed;
         transform.Translate(velocity * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, Settings.LeftBound, Settings.RightBound), Mathf.Clamp(transform.position.y, Settings.BottomBound, Settings.TopBound), transform.position.z);
     }
 }
