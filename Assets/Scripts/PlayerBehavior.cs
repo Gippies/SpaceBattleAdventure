ce44a3f03029d1c386925ad69ba4709664e15982
Joshua Patterson <jleegippies@gmail.com>
2020-03-26 20:48:22 -0500

    Moved constants to playerbehavior

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour {
 
     private const float Speed = 10.0f;
-    
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+    private const float LeftBound = -10.0f;
+    private const float RightBound = 10.0f;
+    private const float BottomBound = -8.0f;
+    private const float TopBound = 8.0f;
 
     // Update is called once per frame
     void Update() {
         Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * Speed;
         transform.Translate(velocity * Time.deltaTime);
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, Settings.LeftBound, Settings.RightBound), Mathf.Clamp(transform.position.y, Settings.BottomBound, Settings.TopBound), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, LeftBound, RightBound), Mathf.Clamp(transform.position.y, BottomBound, TopBound), transform.position.z);
     }
 }
--- Assets/Scripts/Settings.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Settings {
-    public const float LeftBound = -10.0f;
-    public const float RightBound = 10.0f;
-    public const float BottomBound = -8.0f;
-    public const float TopBound = 0.0f;
-}
