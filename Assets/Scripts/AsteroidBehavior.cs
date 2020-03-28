ef106d890e444475141c05b9c44c47418fb1bb18
Joshua Patterson <jleegippies@gmail.com>
2020-03-28 12:19:59 -0500

    Added asteroids and changed bullet speed

--- Assets/Scripts/AsteroidBehavior.cs
+++ Assets/Scripts/AsteroidBehavior.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBehavior : MonoBehaviour {
-    private const float Speed = 20.0f;
+public class AsteroidBehavior : MonoBehaviour {
+    private const float Speed = 3.0f;
     
     // Update is called once per frame
     void Update() {
-        Vector3 velocity = Vector3.up * Speed;
+        Vector3 velocity = Vector3.down * Speed;
         transform.Translate(velocity * Time.deltaTime, Space.Self);
 
-        if (transform.position.y > 50.0f) {
+        if (transform.position.y < -50.0f) {
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidSpawner : MonoBehaviour {
+    public GameObject asteroid;
+    
+    private const float MaxSpawnTime = 5.0f;
+    private const float LeftBound = -20.0f;
+    private const float RightBound = 20.0f;
+    private const float TopBound = 15.0f;
+
+    private float _timeRemaining;
+
+    void Start() {
+        _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_timeRemaining > 0.0f) {
+            _timeRemaining -= Time.deltaTime;
+        }
+        else {
+            Instantiate(asteroid, new Vector3(Random.Range(LeftBound, RightBound), TopBound, 0.0f), Quaternion.identity);
+            _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+        }
+    }
+}
