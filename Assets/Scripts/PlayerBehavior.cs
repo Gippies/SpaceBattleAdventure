8e5b50d0fec1ab3c570c663c75b2bf7fcff0300d
Joshua Patterson <jleegippies@gmail.com>
2020-03-28 11:58:44 -0500

    Changed the bounds to be bigger

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
 
-    public GameObject Bullet;
+    public GameObject bullet;
 
     private const float Speed = 10.0f;
-    private const float LeftBound = -10.0f;
-    private const float RightBound = 10.0f;
+    private const float LeftBound = -20.0f;
+    private const float RightBound = 20.0f;
     private const float BottomBound = -8.0f;
     private const float TopBound = 8.0f;
 
     private void UpdatePosition() {
         Vector3 velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * Speed;
         transform.Translate(velocity * Time.deltaTime);
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, LeftBound, RightBound), Mathf.Clamp(transform.position.y, BottomBound, TopBound), transform.position.z);
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(Mathf.Clamp(pos.x, LeftBound, RightBound), Mathf.Clamp(pos.y, BottomBound, TopBound), pos.z);
     }
 
     private void FireBullets() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + Bullet.transform.localScale.y * 2, transform.position.z);
-            Instantiate(Bullet, bulletPosition, Quaternion.identity);
+            Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + bullet.transform.localScale.y * 2, transform.position.z);
+            Instantiate(bullet, bulletPosition, Quaternion.identity);
         }
     }
 
