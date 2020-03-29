3fd7200ddeeb27e4b885da955ac98c2ee86c87ee
Joshua Patterson <jleegippies@gmail.com>
2020-03-29 12:39:04 -0500

    Added ability to reset the game after game over by pressing the 'R' key

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     
     public Text gameOverText;
+
+    private bool _isGameOver;
     
     public void GameOver() {
         gameOverText.gameObject.SetActive(true);
+        _isGameOver = true;
     }
 
     private void Start() {
         gameOverText.gameObject.SetActive(false);
+        _isGameOver = false;
+    }
+
+    private void Update() {
+        if (_isGameOver && Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
