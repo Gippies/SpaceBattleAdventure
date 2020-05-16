0496e976843f968a21059dd4388098527ba96fe4
Joshua Patterson <jleegippies@gmail.com>
2020-05-15 22:40:12 -0500

    Added asteroid rotation

--- Assets/Scripts/Enemies/AsteroidBehavior.cs
+++ Assets/Scripts/Enemies/AsteroidBehavior.cs
@@ -4,16 +4,25 @@ namespace Enemies {
     public class AsteroidBehavior : MonoBehaviour {
     
         private const float Speed = 3.0f;
+        private const float RotationSpeed = 80.0f;
+
+        private Vector3 _rotationDirection;
 
         private void UpdatePosition() {
             Vector3 velocity = Vector3.down * Speed;
-            transform.Translate(velocity * Time.deltaTime, Space.Self);
+            Vector3 rotationVelocity = _rotationDirection * RotationSpeed;
+            transform.Translate(velocity * Time.deltaTime, Space.World);
+            transform.Rotate(rotationVelocity * Time.deltaTime);
         }
 
         private void DestroyBehavior() {
             if (transform.position.y < Constants.FieldBottomDeSpawn) {
                 Destroy(gameObject);
             }
+        }
+
+        private void Start() {
+            _rotationDirection = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         }
 
         private void Update() {
