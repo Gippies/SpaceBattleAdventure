66144a17b02b4796508582b7ec88b343dee2b858
Joshua Patterson <jleegippies@gmail.com>
2020-05-15 21:28:27 -0500

    Moved spawners to proper namespace

--- Assets/Scripts/Spawners/AsteroidSpawner.cs
+++ Assets/Scripts/Spawners/AsteroidSpawner.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class AsteroidSpawner : MonoBehaviour {
+namespace Spawners {
+    public class AsteroidSpawner : MonoBehaviour {
     
-    public GameObject asteroid;
+        public GameObject asteroid;
     
-    private const float MaxSpawnTime = 5.0f;
+        private const float MaxSpawnTime = 5.0f;
 
-    private float _timeRemaining;
+        private float _timeRemaining;
 
-    private void Start() {
-        _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
-    }
-
-    // Update is called once per frame
-    private void Update() {
-        if (_timeRemaining > 0.0f) {
-            _timeRemaining -= Time.deltaTime;
-        }
-        else {
-            Instantiate(asteroid, new Vector3(Random.Range(Constants.FieldLeftBound, Constants.FieldRightBound), Constants.FieldTopSpawn, 0.0f), Quaternion.identity);
+        private void Start() {
             _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+        }
+
+        // Update is called once per frame
+        private void Update() {
+            if (_timeRemaining > 0.0f) {
+                _timeRemaining -= Time.deltaTime;
+            }
+            else {
+                Instantiate(asteroid, new Vector3(Random.Range(Constants.FieldLeftBound, Constants.FieldRightBound), Constants.FieldTopSpawn, 0.0f), Quaternion.identity);
+                _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+            }
         }
     }
 }
--- Assets/Scripts/Spawners/FighterSpawner.cs
+++ Assets/Scripts/Spawners/FighterSpawner.cs
@@ -1,25 +1,27 @@
 ﻿using UnityEngine;
 
-public class FighterSpawner : MonoBehaviour {
+namespace Spawners {
+    public class FighterSpawner : MonoBehaviour {
     
-    public GameObject fighter;
+        public GameObject fighter;
     
-    private const float MaxSpawnTime = 5.0f;
+        private const float MaxSpawnTime = 5.0f;
 
-    private float _timeRemaining;
+        private float _timeRemaining;
 
-    private void Start() {
-        _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
-    }
-
-    // Update is called once per frame
-    private void Update() {
-        if (_timeRemaining > 0.0f) {
-            _timeRemaining -= Time.deltaTime;
-        }
-        else {
-            Instantiate(fighter, new Vector3(Random.Range(Constants.FieldLeftBound, Constants.FieldRightBound), Constants.FieldTopSpawn, 0.0f), Quaternion.Euler(new Vector3(90, 0, 180)));
+        private void Start() {
             _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+        }
+
+        // Update is called once per frame
+        private void Update() {
+            if (_timeRemaining > 0.0f) {
+                _timeRemaining -= Time.deltaTime;
+            }
+            else {
+                Instantiate(fighter, new Vector3(Random.Range(Constants.FieldLeftBound, Constants.FieldRightBound), Constants.FieldTopSpawn, 0.0f), Quaternion.Euler(new Vector3(90, 0, 180)));
+                _timeRemaining = Random.Range(0.0f, MaxSpawnTime);
+            }
         }
     }
 }
